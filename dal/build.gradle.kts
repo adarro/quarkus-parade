5e988457909a4b57ad2bc8b788d5b672625a5179
adarro <adarro@gmail.com>
2024-08-12 20:13:03 -0600

    Add xodus reference

--- dal/build.gradle.kts
+++ dal/build.gradle.kts
@@ -10,5 +10,8 @@ dependencies {
     implementation("io.quarkus:quarkus-reactive-pg-client")
     implementation("io.quarkus:quarkus-resteasy-reactive")
     implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
+    // lightweight alternative to hibernate-reactive-panache
+    implementation("org.jetbrains.xodus:xodus-openAPI:2.0.1")
+    implementation(libs.jetbrains.xodus.xodus.openAPI)
     testImplementation(libs.testing.assertj)
 }
